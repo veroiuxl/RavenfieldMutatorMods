249d6ff7835b27c56289f65181311bca2b3122ce
veroiuxl <62386060+veroiuxl@users.noreply.github.com>
2020-05-06 18:13:18 +0200

    Create ChatFeedFunction.lua

---
+++ ChatFeedFunction.lua
@@ -0,0 +1,29 @@
+    -- Example:
+    -- Just paste this into the Start function of your behaviour and it will overwrite the chat messages!
+    local ChatFeed = GameObject.Find("ChatFeedScript(Clone)")
+    if ChatFeed == nil then
+        print("ChatFeed not found!")
+    else
+        print("ChatFeed found!")
+        local behaviourChatFeed = ScriptedBehaviour.GetScript(ChatFeed)
+        behaviourChatFeed:ClearKilledMessage()
+        behaviourChatFeed:ClearKillerMessages()
+        behaviourChatFeed:ClearTeamKillerMessages()
+        behaviourChatFeed:ClearTeamKilledMessages()
+        behaviourChatFeed:AddKilledMessage("Hello from Killed")
+        behaviourChatFeed:AddKillerMessage("Hello from Killer")
+        behaviourChatFeed:AddTeamKillerMessage("Hello from Teamkiller")
+        behaviourChatFeed:AddTeamKilledMessage("Hello from Teamkilled")
+        behaviourChatFeed:PushMessage(Player.actor,"asdasdasdasd")
+
+    end
+    -- Functions
+ClearKilledMessage
+ClearKillerMessages
+ClearTeamKillerMessages
+ClearTeamKilledMessages
+
+AddKilledMessage
+AddKillerMessage
+AddTeamKillerMessage
+AddTeamKilledMessage
