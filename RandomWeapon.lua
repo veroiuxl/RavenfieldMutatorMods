90ca85e4da7be08b8f497247d7287ddd50aad516
veroiuxl <62386060+veroiuxl@users.noreply.github.com>
2020-06-06 10:35:02 +0200

    Update RandomWeapon.lua

--- RandomWeapon.lua
+++ RandomWeapon.lua
@@ -2,8 +2,18 @@
 behaviour("RandomWeapon")
 local allWeapons
 function RandomWeapon:Start()
-    GameEvents.onActorSelectedLoadout.AddListener(self,"ActorSelectLoadout")
-    GameEvents.onActorSpawn.AddListener(self,"ActorSpawn")
+    if self.script.mutator.GetConfigurationDropdown("selectedVersion") == 0 then
+        GameEvents.onActorSpawn.AddListener(self,"ActorSpawn")
+    end
+    if self.script.mutator.GetConfigurationDropdown("selectedVersion") == 1 then
+        GameEvents.onActorSpawn.AddListener(self,"ActorSpawnAISelected")
+    end
+    if self.script.mutator.GetConfigurationDropdown("selectedVersion") == 2 then
+        self.script.StartCoroutine("RandomizePerMinutes")
+        print("Will wait " .. tostring(self.script.mutator.GetConfigurationFloat("waitMinutes") * 60) .. " seconds")
+    end
+
+   -- GameEvents.onActorSelectedLoadout.AddListener(self,"ActorSelectLoadout")
      allGear = {}
      allBigGear = {}
      allPrimary = {}
@@ -26,6 +36,68 @@ function RandomWeapon:Start()
 
     end
 
+end
+function RandomWeapon:RandomizePerMinutes()
+    if not Player.actor.isDead then
+    Player.actor.RemoveWeapon(0)
+    Player.actor.RemoveWeapon(1)
+    Player.actor.RemoveWeapon(2)
+    Player.actor.RemoveWeapon(3)
+    Player.actor.RemoveWeapon(4)
+    Player.actor.EquipNewWeaponEntry(allGear[Random.Range(1,#allGear)], 2, false)
+    Player.actor.EquipNewWeaponEntry(allBigGear[Random.Range(1,#allBigGear)], 3, false)
+    Player.actor.EquipNewWeaponEntry(allPrimary[Random.Range(1,#allPrimary)], 0, false)
+    Player.actor.EquipNewWeaponEntry(allSecondary[Random.Range(1,#allSecondary)], 1, false)
+    print("\n")
+    for i,y in ipairs(Player.actor.weaponSlots) do
+        print("Selected Weapon: " .. y.weaponEntry.name .. " in " .. tostring(y.weaponEntry.slot) )
+    end
+    coroutine.yield(WaitForSeconds(self.script.mutator.GetConfigurationFloat("waitMinutes") * 60))
+    self.script.StartCoroutine("RandomizePerMinutes")
+    else
+        coroutine.yield(WaitForSeconds(3))
+        self.script.StartCoroutine("RandomizePerMinutes")
+    end
+
+end
+function RandomWeapon:Randomize(actor)
+
+    return function()
+        local aliveActors = ActorManager.GetAliveActorsOnTeam(Player.team)
+        local randomActor = aliveActors[Random.Range(0,#aliveActors)]
+        if #aliveActors == 0 or randomActor == nil then
+            coroutine.yield(WaitForSeconds(1))
+            self.script.StartCoroutine(self:Randomize(actor))
+            return
+        end
+        local WeaponsActor = randomActor
+        while randomActor == Player.actor do
+            coroutine.yield(WaitForSeconds(1))
+            self.script.StartCoroutine(self:Randomize(actor))
+            return
+        end
+        if WeaponsActor == nil then
+            coroutine.yield(WaitForSeconds(1))
+            self.script.StartCoroutine(self:Randomize(actor))
+            return
+        end
+        local allWeapons = WeaponsActor.weaponSlots
+        for i,y in ipairs(allWeapons) do
+            local weapon = y.weaponEntry
+            actor.RemoveWeapon(y.slot)
+            actor.EquipNewWeaponEntry(weapon, y.slot, false)
+    
+        end 
+        print("\n")
+        for i,y in ipairs(actor.weaponSlots) do
+            print("Selected AI Weapon: " .. y.weaponEntry.name.. " in "  .. tostring(y.weaponEntry.slot) )
+        end
+    end
+    end
+function RandomWeapon:ActorSpawnAISelected(actor)
+    if not actor.isBot then
+        self.script.StartCoroutine(self:Randomize(actor))
+    end
 end
 function RandomWeapon:ActorSpawn(actor)
     if not actor.isBot then
@@ -55,6 +127,8 @@ function RandomWeapon:ActorSelectLoadout(actor,loadout,strategy)
 -- print("Set random loadout")
 --     end
 end
+
+
 function RandomWeapon:Update()
 		
 end
