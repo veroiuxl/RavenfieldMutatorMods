52ca922c5a8928b1216470bc750cb16afb8b77e1
Chryses <62386060+veroiuxl@users.noreply.github.com>
2020-07-19 08:57:02 +0200

    Update VehicleSpawnerUI.lua

--- VehicleSpawnerUI.lua
+++ VehicleSpawnerUI.lua
@@ -1,55 +1,62 @@
 -- Register the behaviour
 behaviour("VehicleSpawnerUI")
-
-local isActive = false
-local canvasVehicleSpawner
-local scriptVar
-local animatorComponent
-local canTriggerAgain = true
-local hud
-local dropDownMenu
-local teamDropDownMenu
-local healthSlider
-local healthText
-local selectedVehicle
-local currentTeam
-local listOfTurrets = {}
-local listOfVehicles = {}
--- Will be replaced
 function VehicleSpawnerUI:Start()
+self.isActive = false
+self.dropDownMenu = nil
+self.selectedVehicle = nil
+self.listOfTurrets = {}
+self.cstmKey = KeyCode.B
+self.listOfVehicles = {}
 	-- Run when behaviour is created
-	self.canvasVehicleSpawner = self.targets.canvasTar
-	self.dropDownMenu = self.targets.dropdownMenu.GetComponent(Dropdown)
-	self.scriptvar = self.script
-	self.healthText = self.targets.healthText
-	self.healthSlider = self.targets.healthSlider
-	self.teamDropDownMenu = self.targets.teamdropdown
-
-	self.targets.buttonTar.GetComponent(Button).onClick.AddListener(self,"ButtonPress")
-	self.targets.selectedTeamButton.GetComponent(Button).onClick.AddListener(self,"SelTeamButton")
+self.canvasVehicleSpawner = self.targets.canvasTar
+self.dropDownMenu = self.targets.dropdownMenu.GetComponent(Dropdown)
+self.scriptvar = self.script
+self.targets.buttonTar.GetComponent(Button).onClick.AddListener(self,"ButtonPress")
+self.targets.selectedTeamButton.GetComponent(Button).onClick.AddListener(self,"SelTeamButton")
 	--self.teamDropDownMenu.GetComponent(Dropdown).onValueChanged.AddListener(self,"TeamDropDownEvent")
 	--self.targets.healthSlider.GetComponent(Slider).onValueChanged.AddListener(self,"oHSVC")
 	--self.targets.dropdownMenu.GetComponent(Dropdown).onValueChanged.AddListener(self,"oVC")
-	isActive = false
-	currentTeam = Player.actor.team
-	if currentTeam == Team.Blue then
-		self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Eagle"
-		self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(0,0,1)
-		else
-			self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Raven"
-		self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(1,0,0)
+self.isActive = false
+self.currentTeam = Player.actor.team
+if self.currentTeam == Team.Blue then
+	self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Eagle"
+	self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(0,0,1)
+	else
+	self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Raven"
+	self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(1,0,0)
+end
+self.canvasVehicleSpawner.setActive(false)
+self.script.StartCoroutine("GetVehicles")
+self.script.StartCoroutine("getKeyFromString")
+end
+
+function VehicleSpawnerUI:getKeyFromString()
+
+	coroutine.yield(WaitForSeconds(1.3))
+
+	local go = GameObject.Find("Custom Keybinds")
+
+	if(go ~= nil) then
+		local customKB = ScriptedBehaviour.GetScript(go)
+
+		local key = self.script.mutator.GetConfigurationString("ctmKey")
+		if(key ~= nil or key ~= "") then
+			local kcode = customKB:getKeyCodeFromCustomKey(key)
+
+			if(kcode ~= nil and kcode ~= KeyCode.None) then
+				self.cstmKey = kcode
+			end
+		end
 	end
-	self.canvasVehicleSpawner.setActive(false)
-	self.script.StartCoroutine("GetVehicles")
 
 end
 function VehicleSpawnerUI:SelTeamButton()
-	if currentTeam == Team.Blue then
-		currentTeam = Team.Red
+	if self.currentTeam == Team.Blue then
+		self.currentTeam = Team.Red
 		self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Raven"
 		self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(1,0,0)
-	else if currentTeam == Team.Red then
-		currentTeam = Team.Blue
+	else if self.currentTeam == Team.Red then
+		self.currentTeam = Team.Blue
 		self.targets.selectedTeamButton.GetComponentInChildren(Text).text = "Eagle"
 		self.targets.selectedTeamButton.GetComponentInChildren(Text).color = Color(0,0,1)
 	end
@@ -58,32 +65,33 @@ function VehicleSpawnerUI:SelTeamButton()
 end
 function VehicleSpawnerUI:GetVehicles()
 	local tempArray = {}
-	for k in pairs(listOfVehicles) do
-		listOfVehicles[k] = nil
+	for k in pairs(self.listOfVehicles) do
+		self.listOfVehicles[k] = nil
 	end
-	for k in pairs(listOfTurrets) do
-		listOfTurrets[k] = nil
+	for k in pairs(self.listOfTurrets) do
+		self.listOfTurrets[k] = nil
 	end
-	local jeep = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.Jeep)
-	local quad = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.Quad)
-	local tank = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.Tank)
-	local ah = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.AttackHelicopter)
-	local ap = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.AttackPlane)
-	local rhib = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.Rhib)
-	local ab = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.AttackBoat)
-	local bp = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.BomberPlane)
-	local th = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.TransportHelicopter)
-	local apc = VehicleSpawner.GetPrefab(currentTeam,VehicleSpawnType.Apc)
+	local jeep = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.Jeep)
+	local quad = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.Quad)
+	local jeepMG = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.JeepMachineGun)
+	local tank = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.Tank)
+	local ah = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.AttackHelicopter)
+	local ap = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.AttackPlane)
+	local rhib = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.Rhib)
+	local ab = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.AttackBoat)
+	local bp = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.BomberPlane)
+	local th = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.TransportHelicopter)
+	local apc = VehicleSpawner.GetPrefab(self.currentTeam,VehicleSpawnType.Apc)
 
-	local mg = TurretSpawner.GetPrefab(currentTeam,TurretSpawnType.MachineGun)
-	local at = TurretSpawner.GetPrefab(currentTeam,TurretSpawnType.AntiTank)
-	local aa = TurretSpawner.GetPrefab(currentTeam,TurretSpawnType.AntiAir)
+	local mg = TurretSpawner.GetPrefab(self.currentTeam,TurretSpawnType.MachineGun)
+	local at = TurretSpawner.GetPrefab(self.currentTeam,TurretSpawnType.AntiTank)
+	local aa = TurretSpawner.GetPrefab(self.currentTeam,TurretSpawnType.AntiAir)
 	self.dropDownMenu.ClearOptions()
 	-- I didn't use a loop because I wanted to make sure that there is no problem with it
 	if jeep ~= nil then
 		local newTempArray = { jeep.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, jeep)
+		table.insert(self.listOfVehicles, jeep)
 	else
 		print("<color=red>Jeep was nil</color>")
 		
@@ -91,7 +99,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if quad ~= nil then
 		local newTempArray = { quad.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, quad)
+		table.insert(self.listOfVehicles, quad)
 	else
 		print("<color=red>quad was nil</color>")
 		
@@ -100,7 +108,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if tank ~= nil then
 		local newTempArray = { tank.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, tank)
+		table.insert(self.listOfVehicles, tank)
 	else
 		print("<color=red>tank was nil</color>")
 		
@@ -108,7 +116,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if ah ~= nil then
 		local newTempArray = { ah.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, ah)
+		table.insert(self.listOfVehicles, ah)
 	else
 		print("<color=red>ah was nil</color>")
 		
@@ -116,7 +124,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if ap ~= nil then
 		local newTempArray = { ap.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, ap)
+		table.insert(self.listOfVehicles, ap)
 	else
 		print("<color=red>ap was nil</color>")
 		
@@ -124,7 +132,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if rhib ~= nil then
 		local newTempArray = { rhib.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, rhib)
+		table.insert(self.listOfVehicles, rhib)
 	else
 		print("<color=red>rhib was nil</color>")
 		
@@ -132,7 +140,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if ab ~= nil then
 		local newTempArray = { ab.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, ab)
+		table.insert(self.listOfVehicles, ab)
 	else
 		print("<color=red>ab was nil</color>")
 		
@@ -140,7 +148,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if bp ~= nil then
 		local newTempArray = { bp.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, bp)
+		table.insert(self.listOfVehicles, bp)
 	else
 		print("<color=red>bp was nil</color>")
 		
@@ -148,7 +156,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if th ~= nil then
 		local newTempArray = { th.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, th)
+		table.insert(self.listOfVehicles, th)
 	else
 		print("<color=red>th was nil</color>")
 		
@@ -156,7 +164,7 @@ function VehicleSpawnerUI:GetVehicles()
 	if apc ~= nil then
 		local newTempArray = { apc.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, apc)
+		table.insert(self.listOfVehicles, apc)
 	else
 		print("<color=red>apc was nil</color>")
 		
@@ -164,17 +172,26 @@ function VehicleSpawnerUI:GetVehicles()
 	if mg ~= nil then
 		local newTempArray = { mg.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, mg)
-		table.insert(listOfTurrets, mg)
+		table.insert(self.listOfVehicles, mg)
+		table.insert(self.listOfTurrets, mg)
 	else
 		print("<color=red>mg was nil</color>")
+		
+	end
+	if jeepMG ~= nil then
+		local newTempArray = { jeepMG.gameObject.name }
+		self.dropDownMenu.AddOptions(newTempArray)
+		table.insert(self.listOfVehicles, jeepMG)
+		table.insert(self.listOfTurrets, jeepMG)
+	else
+		print("<color=red>jeepMG was nil</color>")
 		
 	end
 	if at ~= nil then
 		local newTempArray = { at.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, at)
-		table.insert(listOfTurrets, at)
+		table.insert(self.listOfVehicles, at)
+		table.insert(self.listOfTurrets, at)
 	else
 		print("<color=red>at was nil</color>")
 		
@@ -182,8 +199,8 @@ function VehicleSpawnerUI:GetVehicles()
 	if aa ~= nil then
 		local newTempArray = { aa.gameObject.name }
 		self.dropDownMenu.AddOptions(newTempArray)
-		table.insert(listOfVehicles, aa)
-		table.insert(listOfTurrets, aa)
+		table.insert(self.listOfVehicles, aa)
+		table.insert(self.listOfTurrets, aa)
 	else
 		print("<color=red>aa was nil</color>")
 		
@@ -193,49 +210,38 @@ function VehicleSpawnerUI:GetVehicles()
 	-- for i= 1, #tempArray, 1 do
 	-- 	table.insert(newTempArray, tempArray[i].gameObject.name)
 	-- 	print("Added " .. tempArray[i].gameObject.name .. " to newTempArray")-- DEBUG
-	-- table.insert(listOfVehicles,  tempArray[i])
-	-- print("Added " .. tostring(tempArray[i]) .. " to listOfVehicles") -- DEBUG
+	-- table.insert(self.listOfVehicles,  tempArray[i])
+	-- print("Added " .. tostring(tempArray[i]) .. " to self.listOfVehicles") -- DEBUG
 	-- self.dropDownMenu.AddOptions(newTempArray)
 	-- table.remove(newTempArray, 1)
 	-- end
 
 
 end
-function VehicleSpawnerUI:oVC()
-
---healthSlider.maxValue = selectedVehicle.
-
-end
-function VehicleSpawnerUI:TeamDropDownEvent(value)
-	if value == 0 then 
-		self.dropDownMenu.ClearOptions()
-		self.script.StartCoroutine("GetVehicles")
-	
-	end
-end
-function VehicleSpawnerUI:oHSVC()
-	healthText.text = "Vehicle Health: " .. tostring(healthSlider.value)
-
-end
 function VehicleSpawnerUI:ButtonPress()
 	local ray = Ray(PlayerCamera.activeCamera.main.transform.position + PlayerCamera.activeCamera.main.transform.forward * 1, PlayerCamera.activeCamera.main.transform.forward)
 	local raycast = Physics.Raycast(ray,50, RaycastTarget.ProjectileHit)
+	self.selectedVehicle = nil
 	if raycast ~= nil then
-		for i,y in ipairs(listOfVehicles) do 
+		for i,y in ipairs(self.listOfVehicles) do 
 			if y.gameObject.name == self.dropDownMenu.captionText.text then
-				selectedVehicle = y.gameObject
+				self.selectedVehicle = y.gameObject
 			end
 		end
-		local vehicle = GameObject.Instantiate(selectedVehicle)
+		if(self.selectedVehicle == nil) then
+			print("self.selectedVehicle was nil. No " .. tostring(self.dropDownMenu.captionText.text) .. " found")
+			return
+		end
+		local vehicle = GameObject.Instantiate(self.selectedVehicle)
 		local isTurret = false
 		if vehicle ~= nil then
 		
 		local pos = raycast.point - PlayerCamera.activeCamera.main.transform.forward
 		local vehicletransform = vehicle.transform
-		for z,t in ipairs(listOfTurrets) do
-			if selectedVehicle.gameObject.name == t.gameObject.name then
+		for z,t in ipairs(self.listOfTurrets) do
+			if self.selectedVehicle.gameObject.name == t.gameObject.name then
 				isTurret = true;
-				--print(selectedVehicle.gameObject.name .. " is Turret")
+				--print(self.selectedVehicle.gameObject.name .. " is Turret")
 			end
 		end
 		if not isTurret then
@@ -247,11 +253,12 @@ function VehicleSpawnerUI:ButtonPress()
 		direction = Vector3(direction.x,0,direction.z) 
 		vehicle.transform.rotation = Quaternion.LookRotation(direction)
 		vehicle.transform.position = pos
+		print("Spawned Vehicle at " .. tostring(pos))
 		else
-
 			print("<color=red>Vehicle not found</color>")
 		end
-
+	else
+		print("Raycast was nil")
 	end
 
 end
@@ -261,19 +268,19 @@ function VehicleSpawnerUI:Update()
 --		Player.actor.activeVehicle.gameObject.GetComponent(Rigidbody).AddRelativeTorque( Vector3(0, 0, 500), ForceMode.Impulse);
 --	end
 	
-	if Input.GetKeyDown(KeyCode.B) then
+	if Input.GetKeyDown(self.cstmKey) then
 		--print(#ActorManager.vehicles)
-		if isActive then
+		if self.isActive then
 		--	self.targets.anim.SetBool("uian",false)
 		Screen.UnlockCursor()
 			--self.targets.anim.SetBool("uian",false)
 			self.canvasVehicleSpawner.setActive(false)
-			isActive = false
+			self.isActive = false
 			Screen.LockCursor()
 	
 		else
 			self.canvasVehicleSpawner.setActive(true)
-			isActive = true
+			self.isActive = true
 			Screen.UnlockCursor()
 		end
 		
